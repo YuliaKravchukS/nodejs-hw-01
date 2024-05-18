118b1046e656ea50b83ef1740d02e49a351af5ef
YuliaKravchukS <ljuljek554@gmail.com>
2024-05-18 22:17:05 +0300

    hw1

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  const fileContent = JSON.parse(await fs.readFile(PATH_DB, 'utf8'));
+  if (!Array.isArray(fileContent)) {
+    throw new Error('Дані бази даних не є масивом');
+  }
+  const updateFileContent = fileContent.filter(() => Math.random() >= 0.5);
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updateFileContent), 'utf8');
+    console.log('Дані успішно додані до файлу.');
+  } catch (err) {
+    console.error('Помилка додавання даних до файлу:', err);
+  }
+};
 
 await thanos();
